95256a9644c933746934cdb9955a047a3d2d9784
marypaduano <maria.paduano@gmail.com>
2023-04-27 15:55:44 +0200

    add route in web

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
+
+Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
+
+Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
+
+Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
+
+Route::put('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
+
+Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -26,6 +38,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('projects', ProjectController::class)->parameters([
+        'projects' => 'project:slug'
+    ]);
 });
 
 require __DIR__.'/auth.php';
